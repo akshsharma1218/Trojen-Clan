a4e2f6745ae053f3c00b60e67f90473e1196695a
akshsharma <akshsharma1218@gmail.com>
2021-01-24 13:21:10 +0530

    testing

--- hoard/urls.py
+++ hoard/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
@@ -11,5 +13,4 @@ urlpatterns = [
     path('product/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product-update'),
     path('product/<int:pk>',views.ProductDetailView.as_view(), name='product-detail'),
     path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='post-delete'),
-
 ]
