b689d0b1b1794c7a6c6251def6c6c3236f39dec8
akshsharma <akshsharma1218@gmail.com>
2021-02-07 15:15:27 +0530

    over

--- hoard/urls.py
+++ hoard/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('product/new/', views.ProductCreateView.as_view(), name='product-create'),
     path('product/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product-update'),
     path('product/<int:pk>',views.ProductDetailView.as_view(), name='product-detail'),
-    path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='post-delete'),
+    path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product-delete'),
 ]
