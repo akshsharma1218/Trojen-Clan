70b9b5e0bb60deb254dba8d96d0276d5fca23d19
akshsharma <akshsharma1218@gmail.com>
2021-03-05 23:35:25 +0530

    final

--- b_hoard/settings.py
+++ b_hoard/settings.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -63,12 +64,26 @@ WSGI_APPLICATION = 'b_hoard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASE_URL ="postgres://lerxlnneqrmelr:2e3c7002cfb20c0303f9322febb24855df880f72f0827cd45e57247306896940@ec2-54-145-249-177.compute-1.amazonaws.com:5432/d6l478c8ddghcv"
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd6l478c8ddghcv',
+        'USER' :'lerxlnneqrmelr',
+        'PASSWORD' : '2e3c7002cfb20c0303f9322febb24855df880f72f0827cd45e57247306896940',
+        'HOST' : 'ec2-54-145-249-177.compute-1.amazonaws.com',
+        'PORT' : '5432',
     }
 }
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
+
 
 
 # Password validation
