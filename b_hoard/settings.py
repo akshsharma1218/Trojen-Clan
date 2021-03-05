a062ef6dbf6d59e7f5b1370e835807a4adc368d5
akshsharma <akshsharma1218@gmail.com>
2021-03-05 23:27:49 +0530

    reqs

--- b_hoard/settings.py
+++ b_hoard/settings.py
@@ -108,7 +108,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
