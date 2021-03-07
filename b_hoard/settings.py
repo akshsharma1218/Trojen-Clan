12d863dbbdf99408e4974d53a74b17cadf9d2a66
akshsharma <akshsharma1218@gmail.com>
2021-03-07 17:29:44 +0530

    commit

--- b_hoard/settings.py
+++ b_hoard/settings.py
@@ -142,7 +142,7 @@ LOGIN_URL = 'login'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
-                os.path.join(BASE_DIR,'static')
+                os.path.join(BASE_DIR,'static'),
             )
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
