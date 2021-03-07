eb74db451655c0d903e180e07950a551a619c9d3
akshsharma <akshsharma1218@gmail.com>
2021-03-07 20:54:28 +0530

    com

--- b_hoard/settings.py
+++ b_hoard/settings.py
@@ -140,7 +140,7 @@ LOGIN_URL = 'login'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static/staticfiles')
